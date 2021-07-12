6e0288ba856fc3cfd0499e265aefdd418cf8ec67
Edilson Rodrigues dos Santos <46821440+Edilson-Ribeira@users.noreply.github.com>
2021-07-12 16:24:09 -0300

    Update 02
    Incluindo mais funcionalidades.

--- Salvar_Restaurar.cmd
+++ Salvar_Restaurar.cmd
@@ -1,6 +1,6 @@
 @echo off
 mode con:cols=80 lines=50
-title                /**********\Backup e Restaura‡Æo/***************\
+title                /**********\Backup e Restauracao/***************\
 color A
 :INICIO
 cls
@@ -17,94 +17,280 @@ echo                  *  PROGRAMA PARA REALIZAR BACKUP DO USUARIO  *
 echo                  **********************************************
 echo.
 echo.
-echo                  ______________________________
-echo                       [1] - BACKUP
-echo                       [2] - RESTAURAR BACKUP
-echo                       [3] - SAIR
+echo                  ______________________________________________
 echo.
-rem variavel que pegar a opÃ§Ã£o digitada pelo usuario
-set /p opc=ESCOLHA UMA OPCAO:
-
+echo                              [1] - BACKUP
+echo                              [2] - RESTAURAR BACKUP
+echo                              [3] - BACKUP DRIVERS WINDOWS 10
+echo                              [4] - SAIR
+echo.                 ______________________________________________
 echo.
-
-
-rem condi‡Æo de verifica‡Æo da op‡Æo escolhido pelo usuario
+echo.
+rem variavel que pegar a opção digitada pelo usuario
+set /p opc=                    ESCOLHA UMA OPCAO:
+cls
 if %opc% == 1 goto BACKUP
 if %opc% == 2 goto RESTAURAR
-if %opc% == 3 goto SAIR
-if %opc% GEQ 4 goto INICIO
+if %opc% == 3 goto DRIVER
+if %opc% == 4 goto SAIR
+if %opc% GEQ 5 goto INICIO
 
 rem linha de codigo que faz o backup 
 :BACKUP
 echo.
-echo           EXEMPLO "C:, D:, E:"
-set /p unidade=DIGITE A UNIDADE QUE ESTA OS DADOS:
-rem entrando na para usuarios
-%unidade%
+echo.
+echo       ********  DIGITE O LOGIN DO USUARIO QUE SERA FEITO O BACKUP  ********
+echo.
+echo.
+echo ________________________________________________________________________________
+%HomeDrive%
+cd \Users
+dir
+
+echo.
+echo ________________________________________________________________________________
+echo.
+set /p user=------------  LOGIN DO USUARIO:
+
+cls
+echo.
+echo.
+echo.
+echo         ****** ABAIXO ESTA AS UNIDADE DE DISCO EXISTENTE NO MICRO *******
+echo.
+echo.
+echo ________________________________________________________________________________
+echo.
+C:\Windows\System32\diskpart.exe /s C:\BackupRestauracao\diskp.txt
+echo        ****** SELECIONE A UNIDADE ONDE SERA SALVO O BACKUP DO USUARO  ******
+echo.
+echo                       [1] - D:
+echo                       [2] - E:
+echo                       [3] - F:
+echo                       [4] - G:
+echo                       [5] - SAIR
+echo.
+echo.
+echo ________________________________________________________________________________
+%HomeDrive%
+echo.
+set /p opcDestino=------------    DESTINO:
+cls
+echo.
+echo.
+echo.
+echo              BACKUP INICIALIZADO
+if %opcDestino% == 1 goto Destino1
+if %opcDestino% == 2 goto Destino2
+if %opcDestino% == 3 goto Destino3
+if %opcDestino% == 4 goto Destino4
+if %opcDestino% == 5 goto SAIR
+
+:Destino1
+echo.
+d:
+md users
+cd users
+mkdir %user%
+xcopy %HomeDrive%\Users\%user% %user% /e /c
+echo FIM BACKUP
+echo .
+echo.
+echo @@@@@@@@@@@@@ REALIZADO O BACKUP COM SUCESSO!! @@@@@@@@@@@@@@@@@@@
+pause
+cls
+echo.
+echo.
+echo        ################ INICIANDO O BACKUP DOS DRIVERS #####################
+cd BKP-%user%
+md BKP-DRIVERS
+cd BKP-DRIVERS
+Dism.exe /online /export-driver /destination:d:\BACKUP-USERS\BKP-DRIVERS
+echo.
+echo.
+echo @@@@@@@@@ BACKUP DOS DRIVERS REALIZADO COM SUCESSO @@@@@@@@@@@
+pause
+%HomeDrive%
+goto INICIO
+:Destino2
+e:
+md users
+cd users
+mkdir %user%
+xcopy %HomeDrive%\Users\%user% %user% /e /c
+echo FIM BACKUP
+echo .
+echo.
+echo @@@@@@@@@@@@@@@@@@@@@ REALIZADO O BACKUP COM SUCESSO!! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+pause
+cls
+echo.
+echo.
+echo        ################ INICIANDO O BACKUP DOS DRIVERS #####################
+cd BKP-%user%
+md BKP-DRIVERS
+cd BKP-DRIVERS
+Dism.exe /online /export-driver /destination:e:\BACKUP-USERS\BKP-DRIVERS
+echo.
+echo.
+echo @@@@@@@@@@@@@@@@@@ BACKUP DOS DRIVERS REALIZADO COM SUCESSO @@@@@@@@@@@@@@@@@@@@@@@@@
+%HomeDrive%
+goto INICIO
+
+:Destino3
+f:
+md users
+cd users
+mkdir %user%
+xcopy %HomeDrive%\Users\%user% %user% /e /c
+echo FIM BACKUP
+echo .
+echo.
+echo @@@@@@@@@@@@@@@@@@@@@ REALIZADO O BACKUP COM SUCESSO!! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+pause
+cls
+echo.
+echo.
+echo        ################ INICIANDO O BACKUP DOS DRIVERS #####################
+cd BKP-%user%
+md BKP-DRIVERS
+cd BKP-DRIVERS
+Dism.exe /online /export-driver /destination:f:\BACKUP-USERS\BKP-DRIVERS
+echo.
+echo.
+echo @@@@@@@@@@@@@@@@@@ BACKUP DOS DRIVERS REALIZADO COM SUCESSO @@@@@@@@@@@@@@@@@@@@@@@@@
+%HomeDrive%
+goto INICIO
+
+:Destino4
+g:
+md users
+cd users
+mkdir %user%
+xcopy %HomeDrive%\Users\%user% %user% /e /c
+echo FIM BACKUP
+echo .
+echo.
+echo @@@@@@@@@@@@@@@@@@@@@ REALIZADO O BACKUP COM SUCESSO!! @@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+pause
+cls
+echo.
+echo.
+echo        ################ INICIANDO O BACKUP DOS DRIVERS #####################
+cd BKP-%user%
+md BKP-DRIVERS
+cd BKP-DRIVERS
+Dism.exe /online /export-driver /destination:f:\BACKUP-USERS\BKP-DRIVERS
+echo.
+echo.
+echo @@@@@@@@@@@@@@@@@@ BACKUP DOS DRIVERS REALIZADO COM SUCESSO @@@@@@@@@@@@@@@@@@@@@@@@@
+%HomeDrive%
+goto INICIO
+
+
+rem linha de codigo que faz a restauração do backup do usuario
+
+
+
+:RESTAURAR
+echo.
+echo.
+c:
 cd \Users
 dir
 echo.
-echo DIGITE ABAIXO O LOGIN DO USUARIO
-echo.
-rem variavel que pega o usuario que sera feito o backup
-set /p user= USUARIO:
-echo.
-echo.
-echo AO CRIAR A PASTA ONDE SERA FEITO O BACKUP NÇO UTILIZAR ESPA€O NO NOME DA PASTA
-echo ______________________________________________________________________________
-echo O DIRETORIO NÇO VAI PODER TER ESPA€O, POIS VAI DAR ERRO
-echo ______________________________________________________________________________
-echo DIGITE O DIRETORIO ONDE SERA SALVO OS DADOS DO USUARIO
-echo.
-rem variavel que armazena o diretorio onde sera salvo os dados do usuario
-set /p local= DIRETORIO:
-echo.
-echo REALIZANDO O BACKUP DO COLABORADOR!
-echo.
-rem comando xcopy que salva os dados do usuario
-xcopy %user% %local% /e /c
-echo .
-echo.
-echo REALIZADO O BACKUP COM SUCESSO!!
-pause
+set /p user=------------  LOGIN DO USUARIO:
 cls
-goto INICIO
-
-rem linha de codigo que faz a restauraÃ§Ã£o do backup do usuario
-:RESTAURAR
-echo.
-echo           EXEMPLO "C:, D:, E:"
-set /p unidade=DIGITE A UNIDADE QUE ESTA OS DADOS:
 rem entrando na para usuarios
-%unidade%
-cd Users
-dir
+diskpart /s C:\BackupRestauracao\diskp.txt
 echo.
-echo DIGITE ABAIXO O LOGIN DO USUARIO
+echo         ****** SELECIONE A UNIDADE ONDE ESTA O BACKUP DO USUARIO  ******
 echo.
-rem variavel que pega o usuario que sera feito a restauraÃ§Ã£o do backup
-set /p user= USUARIO:
+echo                       [1] - D:
+echo                       [2] - E:
+echo                       [3] - G:
+echo                       [4] - SAIR
+
+set /p opcOrigem=BKP_Origem:
+
+cls
+if %opcOrigem% == 1 goto Origem
+if %opcOrigem% == 3 goto SAIR
+if %opcOrigem% GEQ 4 goto INICIO
+
+:Origem
+d:
+cd \Users
+pause
 echo.
 echo.
-echo AO CRIAR A PASTA ONDE SERA FEITO O BACKUP NÇO UTILIZAR ESPA€O NO NOME DA PASTA
-echo ______________________________________________________________________________
-echo O DIRETORIO NÇO VAI PODER TER ESPA€O, POIS VAI DAR ERRO
-echo ______________________________________________________________________________
-echo DIGITE O DIRETORIO ONDE SERA SALVO OS DADOS DO USUARIO
 echo.
-rem variavel que armazena o diretorio onde os dados do usuario esta salvo
-set /p local= DIRETORIO:
-echo.
-echo REALIZANDO O BACKUP DO COLABORADOR!
-echo.
-rem comando xcopy que copia os dados salvos para o perfil do usuario
-xcopy %local% %user%  /e /c /y
+xcopy %user% %HomeDrive%\Users\%user% /e /c /y
 echo .
 echo.
-echo REALIZADO A RESTAURA€ÇO DO BACKUP COM SUCESSO!!
 pause
 cls
 goto INICIO
-rem Sai da aplica‡Æo
+rem Sai da aplicação
 :SAIR
 exit
+
+
+rem Realizar backup de drivers do Windows 10
+:DRIVER
+diskpart /s C:\BackupRestauracao\diskp.txt
+echo.
+echo         ****** SELECIONE A UNIDADE ONDE SERA SALVO O BACKUP DOS DRIVERS  ******         
+echo.
+echo                       [1] - D:
+echo                       [2] - E:
+echo                       [3] - G:
+echo                       [4] - SAIR
+echo.
+echo.
+set /p unidade=Digite a unidade onde sera salvo os Drivers:
+echo. 
+echo.
+if %unidade% == 1 goto UNIDADE_D
+if %unidade% == 2 goto UNIDADE_E
+if %unidade% == 3 goto UNIDADE_G
+if %unidade% == 4 goto SAIR
+if %unidade% GEQ 4 goto INICIO
+echo.
+echo.
+:UNIDADE_D
+d:
+echo        ################ INICIANDO O BACKUP DOS DRIVERS #####################
+cd BKP-%user%
+md BKP-DRIVERS
+cd BKP-DRIVERS
+Dism.exe /online /export-driver /destination:d:\BACKUP-USERS\BKP-DRIVERS
+echo.
+echo.
+echo @@@@@@@@@@@@@@@@@@ BACKUP DOS DRIVERS REALIZADO COM SUCESSO @@@@@@@@@@@@@@@@@@@@@@@@@
+echo.
+echo.
+:UNIDADE_E
+e:
+echo        ################ INICIANDO O BACKUP DOS DRIVERS #####################
+cd BKP-%user%
+md BKP-DRIVERS
+cd BKP-DRIVERS
+Dism.exe /online /export-driver /destination:d:\BACKUP-USERS\BKP-DRIVERS
+echo.
+echo.
+echo @@@@@@@@@@@@@@@@@@ BACKUP DOS DRIVERS REALIZADO COM SUCESSO @@@@@@@@@@@@@@@@@@@@@@@@@
+echo.
+echo.
+:UNIDADE_G
+g:
+echo        ################ INICIANDO O BACKUP DOS DRIVERS #####################
+cd BKP-%user%
+md BKP-DRIVERS
+cd BKP-DRIVERS
+Dism.exe /online /export-driver /destination:d:\BACKUP-USERS\BKP-DRIVERS
+echo.
+echo.
+echo @@@@@@@@@@@@@@@@@@ BACKUP DOS DRIVERS REALIZADO COM SUCESSO @@@@@@@@@@@@@@@@@@@@@@@@@
+echo.
+echo.
